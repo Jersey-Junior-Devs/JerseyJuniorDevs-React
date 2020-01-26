ab68ead7a0f46a7773ee51870f3f49c6477e382d
Anca <anca.toma101@gmail.com>
2020-01-26 20:10:32 +0000

    updated join links

--- src/components/Contents/IndexContent.js
+++ src/components/Contents/IndexContent.js
@@ -15,6 +15,7 @@ function IndexContent() {
           <Container>
             <h2 className="title">Join the community</h2>
             <Row>
+
               <Col md="3">
                 <div className="description">
                   <h4
@@ -24,21 +25,19 @@ function IndexContent() {
                     Step 1
                   </h4>
                   <a
-                    href="https://www.facebook.com/groups/3092720600797985/"
+                    href="https://discord.gg/BRXukzU"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <div>
-                      <img
-                        alt="..."
-                        className="img-rounded img-responsive"
-                        src={require("assets/img/icons/facebook.png")}
-                        style={{ width: "80%", padding: "10px" }}
-                      />
-                      <p className="darker pl-5 pr-5">
-                        Join the Jersey Junior Devs Facebook group.
-                      </p>
-                    </div>
+                    <img
+                      alt="..."
+                      className="img-rounded img-responsive"
+                      src={require("assets/img/icons/discord.png")}
+                      style={{ width: "80%" }}
+                    />
+                    <p className="darker pl-5 pr-5">
+                      Join the Discord community, ask questions or help others.
+                    </p>
                   </a>
                 </div>
               </Col>
@@ -76,31 +75,6 @@ function IndexContent() {
                     Step 3
                   </h4>
                   <a
-                    href="https://discord.gg/BRXukzU"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      alt="..."
-                      className="img-rounded img-responsive"
-                      src={require("assets/img/icons/discord.png")}
-                      style={{ width: "80%" }}
-                    />
-                    <p className="darker pl-5 pr-5">
-                      Share ideas or ask for help on the Discord server.
-                    </p>
-                  </a>
-                </div>
-              </Col>
-              <Col md="3">
-                <div className="description">
-                  <h4
-                    className="info-title"
-                    style={{ color: "#F7CB15", fontWeight: "1000" }}
-                  >
-                    Step 4
-                  </h4>
-                  <a
                     href="https://trello.com/invite/b/NVgGPulp/87a2320880adcc71349d89d12ae59915/jjd-website"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -115,6 +89,33 @@ function IndexContent() {
                       Visit the Trello board and start contributing to the
                       website.
                     </p>
+                  </a>
+                </div>
+              </Col>
+              <Col md="3">
+                <div className="description">
+                  <h4
+                    className="info-title"
+                    style={{ color: "#F7CB15", fontWeight: "1000" }}
+                  >
+                    Step 4
+                  </h4>
+                  <a
+                    href="https://www.facebook.com/jerseyjuniordevs/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div>
+                      <img
+                        alt="..."
+                        className="img-rounded img-responsive"
+                        src={require("assets/img/icons/facebook.png")}
+                        style={{ width: "80%", padding: "10px" }}
+                      />
+                      <p className="darker pl-5 pr-5">
+                        Like and share our Facebook page to spread the word.
+                      </p>
+                    </div>
                   </a>
                 </div>
               </Col>
